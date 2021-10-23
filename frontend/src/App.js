7cb9c5c817269c17545f00fb804badde7f7e349c
Christy <57055910+christyc01@users.noreply.github.com>
2021-10-22 23:12:21 -0600

    HomeScreen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from 'react-bootstrap';
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Container className="py-3">
           <h1>E-commerce Site?</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import products from '../products';
+
+const HomeScreen = () => {
+  return ( 
+    <Row>
+      {products.map((product) => {
+        return (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        )
+      })
+      }
+    </Row>
+   );
+}
+ 
+export default HomeScreen;
