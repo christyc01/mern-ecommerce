afa0c33d394664b2613d60f9e027824306d65ab9
Christy <57055910+christyc01@users.noreply.github.com>
2021-10-31 09:31:26 -0600

    Product details reducer and action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Message from '../components/Message';
