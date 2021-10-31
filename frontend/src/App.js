57713a1197e451db0fd5eb710a5fd18810e04c65
Christy <57055910+christyc01@users.noreply.github.com>
2021-10-31 10:25:54 -0600

    Quantity select and add to cart button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from 'react-bootstrap';
-import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Container className="py-3">
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
-
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
